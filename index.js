8f4141de7978df5a50cad364fc79b44f4c5b3033
Simon Lepel <simbo@simbo.de>
2015-09-16 13:44:29 +0200

    export validate-by-shorthand

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 'use strict';
 
 var Options = require('./lib/options.js'),
-    Option = require('./lib/option.js');
+    Option = require('./lib/option.js'),
+    validate = require('validate-by-shorthand');
 
 module.exports = Options;
 module.exports.Option = Option;
